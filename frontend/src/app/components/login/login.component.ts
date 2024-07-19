67bfa0e257338f726d5b862b8e295639ac81ec3e
tabish29 <tabish13295@gmail.com>
2024-07-19 08:05:12 +0200

    aggiunto localstorageService, e il salvataggio dell'username nel localStorage

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { UserService } from '../../services/user.service';
 import { User } from '../../models/User';
 import { Router } from '@angular/router';
+import { LocalStorageService } from '../../services/local-storage.service';
 
 @Component({
   selector: 'app-login',
@@ -12,22 +13,25 @@ export class LoginComponent {
   username: string = '';
   password: string = '';
 
-  constructor(private userService: UserService,private router: Router) {}
-  
+  constructor(private userService: UserService,private router: Router, private localStorageService: LocalStorageService) {}
+
+
   onLogin() {
     this.userService.login(this.username, this.password).subscribe(
       response => {
         console.log('Login successful', response);
+      
+        this.localStorageService.setItem('username', this.username);
 
         this.router.navigateByUrl('/home');
-       
       },
       error => {
         console.error('Login failed', error);
-       
+        
       }
     );
   }
+
 
 
 }
