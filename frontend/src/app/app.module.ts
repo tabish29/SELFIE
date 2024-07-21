665e909d3a44a5b516ce380e009295f568233bbf
tabish29 <tabish13295@gmail.com>
2024-07-21 09:37:56 +0200

    aggiunto angular material

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { LoginComponent } from './components/login/login.component';
 import { RegistrationComponent } from './components/registration/registration.component';
 import { TimeMachineComponent } from './components/time-machine/time-machine.component';
 import { UserNotesComponent } from './components/user-notes/user-notes.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 @NgModule({
   declarations: [
@@ -35,7 +36,8 @@ import { UserNotesComponent } from './components/user-notes/user-notes.component
     HttpClientModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
 })
