f82583fad87c15d759b09ec90f5cf5485e5e476a
AleDeso <alessandrodesolda.ad@gmail.com>
2024-09-04 11:20:05 +0200

    giochi sfondi

--- frontend/src/app/components/game/sudoku/sudoku.component.ts
+++ frontend/src/app/components/game/sudoku/sudoku.component.ts
@@ -3,23 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-sudoku',
   templateUrl: './sudoku.component.html',
-  styleUrls: ['./sudoku.component.css']
+  styleUrl: './sudoku.component.css'
 })
 export class SudokuComponent {
-  sudokuBoxes: Array<Array<Array<{ value: string }>>> = [];
 
-  constructor() {
-    this.initializeBoard();
-  }
-
-  initializeBoard() {
-    this.sudokuBoxes = Array.from({ length: 9 }, () => 
-      Array.from({ length: 3 }, () => 
-        Array.from({ length: 3 }, () => ({ value: '' }))
-      )
-    );
-
-    // Puoi aggiungere qui i valori iniziali per la visualizzazione
-    this.sudokuBoxes[0][0][0].value = '5'; // Esempio: imposta il primo valore del primo quadrato
-  }
 }
