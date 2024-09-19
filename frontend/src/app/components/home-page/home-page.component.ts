bb408c8300be8eb971ab8716826c2dd12a8a147f
AleD <alessandrodesolda.ad@gmail.com>
2024-09-19 16:12:38 +0200

    modifiche home

--- frontend/src/app/components/home-page/home-page.component.ts
+++ frontend/src/app/components/home-page/home-page.component.ts
@@ -7,7 +7,8 @@ import { DragService } from '../../services/drag.service';
   styleUrl: './home-page.component.css'
 })
 export class HomePageComponent {
-  
+  currentVisualMode: number = 0;
+
   constructor(
     private dragService: DragService
   ) { }
@@ -15,6 +16,26 @@ export class HomePageComponent {
   onMouseDown(event: MouseEvent): void {
     const target = event.target as HTMLElement;
     this.dragService.startDrag(event, target);
+  }
+
+  ngOnInit(): void {
+  
+  }
+
+  preNote() {
+    if (this.currentVisualMode > 0) {
+      this.currentVisualMode--;
+    } else {
+      this.currentVisualMode = 3; // Torna all'ultima modalità (ciclo)
+    }
+  }
+
+  nextNote() {
+    if (this.currentVisualMode < 3) {
+      this.currentVisualMode++;
+    } else {
+      this.currentVisualMode = 0; // Ricomincia da 0
+    }
   }
 
 }
--- frontend/src/app/components/home-page/ultima-nota/ultima-nota.component.ts
+++ frontend/src/app/components/home-page/ultima-nota/ultima-nota.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component,Input, OnInit } from '@angular/core';
+import { TimeMachineService } from '../../../services/time-machine.service'; // Importa il servizio
+import { TimeMachine } from '../../../models/TimeMachine';
+import { UserNote } from '../../../models/UserNote';
+import { UserNoteService } from '../../../services/user-note.service';
+import { LocalStorageService } from '../../../services/local-storage.service';
+import { UpdateNoteDialogComponent } from '../../update-note-dialog/update-note-dialog.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-ultima-nota',
@@ -6,5 +13,147 @@ import { Component } from '@angular/core';
   styleUrl: './ultima-nota.component.css'
 })
 export class UltimaNotaComponent {
+  @Input() visualMode: number = 0; // Modalità di visualizzazione corrente
+  notes: UserNote[] = [];
+  selectedNote: UserNote | null = null;
+  authorUsername: string = '';
+  noteDay: UserNote[] = []; // Nuovo array per le note del giorno
 
+  constructor(
+    private userNotesService: UserNoteService,
+    private localStorageService: LocalStorageService,
+    private dialog: MatDialog,
+    private timeMachineService: TimeMachineService
+  ) { }
+
+  ngOnInit(): void {
+    this.authorUsername = this.localStorageService.getItem('username');
+
+    if (this.authorUsername) {
+      this.loadNotesByAuthor(this.authorUsername);
+    } else {
+      console.log("Non esiste l'username dell'autore");
+    }
+  }
+
+  // Metodo per caricare le note dell'autore
+  loadNotesByAuthor(authorUsername: string): void {
+    this.userNotesService.getNotesByAuthor(authorUsername).subscribe(
+      notes => {
+        this.notes = notes;
+        this.selectLatestNote();  // Metodo per selezionare l'ultima nota
+        this.loadNotesForToday(); // Carica le note del giorno
+      },
+      error => console.error('Errore nel caricamento delle note dell\'autore', error)
+    );
+  }
+
+  // Metodo per selezionare l'ultima nota aggiornata
+  selectLatestNote(): void {
+    if (this.notes.length > 0) {
+      this.notes.sort((a, b) => new Date(b.updatedAt).getTime() - new Date(a.updatedAt).getTime());
+      this.selectedNote = this.notes[0]; // Seleziona l'ultima nota aggiornata
+    }
+    console.log('Note:', this.notes);
+  }
+
+  // Metodo per caricare solo le note del giorno
+  loadNotesForToday(): void {
+    // Esegui la chiamata al servizio passando lo username
+    this.timeMachineService.getTimeMachine(this.authorUsername).subscribe(
+      (timeMachine: TimeMachine) => {
+        const today = new Date(timeMachine.date); // Converte la stringa in un oggetto Date
+
+        this.noteDay = this.notes.filter(note => {
+          const noteDate = new Date(note.updatedAt || note.createdAt);
+          
+          // Verifica se la nota è stata creata o aggiornata oggi
+          return noteDate.getDate() === today.getDate() &&
+                noteDate.getMonth() === today.getMonth() &&
+                noteDate.getFullYear() === today.getFullYear();
+        });
+        
+        console.log('Note del giorno:', this.noteDay);
+      },
+      error => {
+        console.error('Errore durante il recupero della time machine:', error);
+      }
+    );
+  }
+
+ 
+  /*Metodo per modificare le Note */
+  selectNote(note: UserNote): void {
+    this.selectedNote = { ...note };
+
+    this.openUpdateNoteDialog(this.selectedNote);
+  }
+  openUpdateNoteDialog(note: UserNote): void {
+    const dialogRef = this.dialog.open(UpdateNoteDialogComponent, {
+      width: '400px',
+      data: note
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+
+        if (result.isDeleted) {
+          this.deleteNote(result.note);
+        }
+        if (result.isUpdated) {
+          this.updateNoteInList(result.note);
+        }
+        if (result.isDuplicated) {
+          this.duplicateNoteInList(result.note);
+        }
+
+      }
+    });
+  }
+  duplicateNoteInList(duplicatedNote: UserNote): void {
+    if (duplicatedNote.authorUsername) {
+      this.userNotesService.createNote(duplicatedNote).subscribe(
+        (createdNote) => {
+          console.log('Nota duplicata con successo');
+          this.notes.push(duplicatedNote);
+        },
+        (error) => console.error('Errore nella duplicazione della nota', error)
+      );
+
+    }
+  }
+  updateNoteInList(updatedNote: UserNote): void {
+    const index = this.notes.findIndex(note => note.title === updatedNote.title);
+    if (index > -1) {
+      this.notes[index] = updatedNote;
+  
+      if (updatedNote.authorUsername) {
+        this.userNotesService.updateNote(updatedNote.authorUsername, updatedNote.title, updatedNote).subscribe(
+          () => console.log('Nota aggiornata con successo'),
+          error => console.error('Errore nell\'aggiornamento della nota', error)
+        );
+      }
+    }
+  }
+  deleteNote(noteToDelete: UserNote): void {
+    const index = this.notes.findIndex(note => note.title === noteToDelete.title);
+    if (index > -1) {
+      this.notes.splice(index, 1);
+    }
+  } 
+  // Modalità di visualizzazione differenti
+  get currentView() {
+    switch (this.visualMode) {
+      case 0:
+        return 'Visualizzazione 1';
+      case 1:
+        return 'Visualizzazione 2';
+      case 2:
+        return 'Visualizzazione 3';
+      case 3:
+        return 'Visualizzazione 4';
+      default:
+        return 'Visualizzazione Predefinita';
+    }
+  }
 }
